a63e37a6a7c4fc40a213deae8f2bb52d2afd18e7
aoeiauuarar <tawangood46@gmail.com>
2026-04-01 09:33:58 +0000

    ID

--- MU_Person.h
+++ MU_Person.h
@@ -21,7 +21,7 @@ MU_person::MU_person(long x,string n):NODE(x){
   // Set up all MU_Person values
   id = x;
   name = n;
-  cout<<"MU person constructor "<<id<<endl;
+  cout<<"MU person constructor  "<<id<<endl;
 }
 
 void MU_person::display_person(){
