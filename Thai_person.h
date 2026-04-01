af7bdd4289c5adcf92c712cd3b6c5c56276abd0f
aoeiauuarar <tawangood46@gmail.com>
2026-04-01 09:29:18 +0000

    ID

--- Thai_person.h
+++ Thai_person.h
@@ -2,6 +2,11 @@ class Thai_person{
 	private: 
 		long nat_id;
 	public: 
-		Thai_person(int=0);
+		Thai_person(int);
  };
+
+ Thai_person::Thai_person(int t){
+	nat_id = t;
+	cout<<"Thai person constructor "<<nat_id<<endl;
+ }
 
--- makefile
+++ makefile
@@ -1,2 +1,8 @@
-target: MU_Person.h student.h main.cpp
-	g++ main.cpp -o inh
+compile: MU_Person.h student.h LL_test.cpp
+	 g++ LL_test.cpp LL.cpp NODE.cpp -o inh
+run: inh
+	 ./inh
+
+
+clean: inh
+	 rm inh
